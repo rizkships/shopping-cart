d49969483c3c28a8b5ef4c32b013283ffa92382d
rizk <rizkdotai@gmail.com>
2024-02-08 18:52:23 +0200

    create navbar link routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import { Container } from "react-bootstrap"
 import {Home} from "./pages/Home"
 import {Store} from "./pages/Store"
 import {About} from "./pages/About"
+import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
     <>
+    <Navbar />
      <Container className="mb-4">
       <Routes>
         <Route path="/" element={<Home />} />
