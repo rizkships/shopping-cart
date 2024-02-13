28c6c239061c843c8c677a67e39e50671c067c4e
rizk <rizkdotai@gmail.com>
2024-02-13 09:34:11 +0200

    add local storage

--- src/components/Cartitem.tsx
+++ src/components/Cartitem.tsx
@@ -1,3 +1,4 @@
+import { Button, Stack } from "react-bootstrap"
 import {useShoppingCart} from "../context/ShoppingCartContext"
 import  storeItems  from "../data/items.json"
 import { formatCurrency } from "../utilities/formatCurrency"
@@ -15,26 +16,32 @@ export function CartItem({id, quantity}) {
     if (item == null) return null 
 
     return (
-        <Stack direction="horizontal" gap={2} className="d-flex-align-items-center">
-            <img src={item.imgUrl}
-            style = {{width: '125px', height: '75px', objectFit:'cover'}}/>
-            <div className="me-auto">
-                <div>
-                    {item.name}{" "}
-                    {quantity > 1 && (
-                        <span className="text-muted" style={{fontSize: ".65rem" }}>
-                            x{quantity}
-                        </span>
-                    )}
-                </div>
-                <div className="text-muted" style={{fontsize: '.75rem'}}>
-                        {formatCurrency(item.price)}
-                </div>
-                <div>
-                    {formatCurrency(item.price * quantity)}
-                </div>
-                <Button variant="outline-danger" size="sm" onClick={() => removeFromCart(item.id)}>&times;</Button>
-            </div>
-        </Stack>
+        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div> {formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
+    </Stack>
     )
 }
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useContext, useReducer } from "react";
 import { useState } from "react";
 import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -33,7 +34,7 @@ export function useShoppingCart() {
 export function ShoppingCartProvider({children}:
     ShoppingCartProviderProps) {
         const [isOpen, setIsOpen] = useState(false)
-        const [cartItems, setCartItems] = useState<CartItem[]>([])
+        const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart",[])
 
         const cartQuantity = cartItems.reduce(
             (quantity, item) => item.quantity + quantity, 0)
